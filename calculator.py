ca4169e7e6892a4aa4605a7af8377ee989b4b7a7
Sejal <vsejal1008@gmail.com>
2026-08-20 18:40:20 +0530

    added calculator.py

---
+++ calculator.py
@@ -0,0 +1,17 @@
+print("===Sejal's Calculator===")
+
+num1=float(input("enter your first number"))
+operator=input("enter your operator")
+num2=float(input("enter your second number"))
+if operator=="+":
+    result = num1+num2
+elif operator=="-":
+    result = num1-num2
+elif operator=="*":
+    result = num1*num2
+elif operator=="/":
+    result = num1/num2 
+else:
+    result ="wrong operator!"
+print("Result:",result)       
+
