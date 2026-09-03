09705c2831d21747825611cddb7bc632488d2ca6
Sejal <vsejal1008@gmail.com>
2026-09-03 20:25:13 +0530

    added minicaht.py

---
+++ minicaht.py
@@ -0,0 +1,7 @@
+print("Hii! i am your minibot")
+q = input("How are you")
+if"bad"in q or"sad"in q:
+    print("don't worry coding cheers you up!")
+else:
+    print("great!let's start coding")
+    
