b5bae33d00733b28de8a4c344f20220e5e538325
Sejal <vsejal1008@gmail.com>
2026-09-03 20:05:01 +0530

    added program.py

---
+++ program.py
@@ -0,0 +1,11 @@
+#STUDENT RESULT CARD
+
+name=input("enter your name")
+marks=int(input("enter your marks(out of 100):"))
+
+if marks >=90:
+    print("{name},grade A+")
+elif marks >=60:
+    print("{name},grade B")
+else:
+    print("{name},grade C")        
