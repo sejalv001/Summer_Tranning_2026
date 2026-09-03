8dcb0727d3eaaba9e00cc6a05e875026837412dc
Sejal <vsejal1008@gmail.com>
2026-09-03 20:34:05 +0530

    added pattern.py

--- minicaht.py
+++ minicaht.py
@@ -1,7 +1,7 @@
+#MINI CHATBOT
 print("Hii! i am your minibot")
 q = input("How are you")
 if"bad"in q or"sad"in q:
     print("don't worry coding cheers you up!")
 else:
     print("great!let's start coding")
-    
